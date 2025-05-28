989bebdc1978ba4ea25eafe4ca5c7abb9302383a
Fauzaan <mfauzaan@icloud.com>
2025-05-28 11:29:31 +0000

    fix: keep the button on disable state for floatign bar

--- pluk/Views/Documents/FloatingActionBar/Pagination.swift
+++ pluk/Views/Documents/FloatingActionBar/Pagination.swift
@@ -12,25 +12,25 @@ struct Pagination: View {
     
     var body: some View {
         HStack(spacing: 0) {
-            if viewModel.currentPage > 1 {
-                Button(action: {
-                    withAnimation(.spring(response: 0.3)) {
-                        viewModel.previousPage()
-                    }
-                }) {
-                    Image(systemName: "chevron.left")
-                        .foregroundColor(.white)
-                        .font(.system(size: 14))
-                        .contentShape(Rectangle())
+            Button(action: {
+                withAnimation(.spring(response: 0.3)) {
+                    viewModel.previousPage()
                 }
-                .buttonStyle(ActionButtonStyle(padding: EdgeInsets(top: 7, leading: 9, bottom: 7, trailing: 9)))
-                .keyboardShortcut(.leftArrow, modifiers: .command)
-                .customHelp("Go to previous page", position: .top, shortcut: KeyboardShortcut(
-                    modifiers: [.command],
-                    key: "←"
-                ))
-                .transition(.scale.combined(with: .opacity))
+            }) {
+                Image(systemName: "chevron.left")
+                    .foregroundColor(.white)
+                    .opacity(viewModel.currentPage <= 1 ? 0.3 : 1)
+                    .font(.system(size: 14))
+                    .contentShape(Rectangle())
             }
+            .disabled(viewModel.currentPage <= 1)
+            .buttonStyle(ActionButtonStyle(padding: EdgeInsets(top: 7, leading: 9, bottom: 7, trailing: 9)))
+            .keyboardShortcut(.leftArrow, modifiers: .command)
+            .customHelp("Go to previous page", position: .top, shortcut: KeyboardShortcut(
+                modifiers: [.command],
+                key: "←"
+            ))
+            .transition(.scale.combined(with: .opacity))
             
             Button(action: {
                 // Open Modal
@@ -38,26 +38,25 @@ struct Pagination: View {
                 Text("\(viewModel.totalItems) documents").foregroundColor(.gray)
             }
             .buttonStyle(ActionButtonStyle(padding: EdgeInsets(top: 7, leading: 8, bottom: 7, trailing: 8), disableScaleEffect: true))
-            
-            if viewModel.currentPage < viewModel.totalPages {
-                Button(action: {
-                    withAnimation(.spring(response: 0.3)) {
-                        viewModel.nextPage()
-                    }
-                }) {
-                    Image(systemName: "chevron.right")
-                        .foregroundColor(.white)
-                        .font(.system(size: 14))
-                        .contentShape(Rectangle())
+            Button(action: {
+                withAnimation(.spring(response: 0.3)) {
+                    viewModel.nextPage()
                 }
-                .buttonStyle(ActionButtonStyle(padding: EdgeInsets(top: 7, leading: 9, bottom: 7, trailing: 9)))
-                .keyboardShortcut(.rightArrow, modifiers: .command)
-                .customHelp( "Go to next page", position: .top, shortcut: KeyboardShortcut(
-                    modifiers: [.command],
-                    key: "→"
-                ))
-                .transition(.scale.combined(with: .opacity))
+            }) {
+                Image(systemName: "chevron.right")
+                    .foregroundColor(.white)
+                    .opacity(viewModel.currentPage >= viewModel.totalPages ? 0.3 : 1)
+                    .font(.system(size: 14))
+                    .contentShape(Rectangle())
             }
+            .disabled(viewModel.currentPage >= viewModel.totalPages)
+            .buttonStyle(ActionButtonStyle(padding: EdgeInsets(top: 7, leading: 9, bottom: 7, trailing: 9)))
+            .keyboardShortcut(.rightArrow, modifiers: .command)
+            .customHelp( "Go to next page", position: .top, shortcut: KeyboardShortcut(
+                modifiers: [.command],
+                key: "→"
+            ))
+            .transition(.scale.combined(with: .opacity))
         }
     }
 }
