e0bc46cab103dd42471a206cd92a2250b8ebb5cb
Fauzaan <mfauzaan@icloud.com>
2025-07-22 16:50:01 +0000

    get build version

--- scripts/create-dmg.sh
+++ scripts/create-dmg.sh
@@ -86,6 +86,7 @@ ln -s /Applications "$DMG_TEMP/Applications"
 # Create initial DMG as read-write
 DMG_RW_PATH="${DMG_PATH%.dmg}-rw.dmg"
 hdiutil create \
+    -volname "Pluk" \
     -srcfolder "$DMG_TEMP" \
     -ov \
     -format UDRW \
