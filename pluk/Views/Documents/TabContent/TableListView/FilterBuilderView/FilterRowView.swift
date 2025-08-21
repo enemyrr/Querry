ff0d57ac82317c3c359e46239d866df6e835f169
Fauzaan <mfauzaan@icloud.com>
2025-08-21 20:40:07 +0500

    130 fix bring back filter operators (#131)
    * intial commit
    
    * fix: fetch and AI
    
    * WIP
    
    * feat: add fitler support
    
    * fix: make sure all data is updated correctly
    
    * bring back fitler row view

--- pluk/Views/Documents/TabContent/TableListView/FilterBuilderView/FilterRowView.swift
+++ pluk/Views/Documents/TabContent/TableListView/FilterBuilderView/FilterRowView.swift
@@ -55,6 +55,9 @@ struct FilterRowView: View {
             .buttonStyle(FilterDropdownStyle())
             .frame(width: 160)
             
+            /// Operator
+            OperatorDropdown(selectedOperator: $condition.filterOperator)
+            
             // Value input
             TextField("Enter a value", text: $condition.value)
                 .textFieldStyle(FilterTextFieldStyle())
